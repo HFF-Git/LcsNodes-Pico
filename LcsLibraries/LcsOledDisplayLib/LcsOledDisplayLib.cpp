4480be8af5aa7c0870e51907d691b7ee88bafd8e
Helmut Fieres <67054756+HFF-Git@users.noreply.github.com>
2024-07-19 10:36:59 +0200

    Added two dummy libs for LCD and OLED displays...
    Still searching a small convenient lib to implement these functions.

---
+++ LcsLibraries/LcsOledDisplayLib/LcsOledDisplayLib.cpp
@@ -0,0 +1,63 @@
+//------------------------------------------------------------------------------------------------------------
+//
+// LCS - OLED Display Driver
+//
+//------------------------------------------------------------------------------------------------------------
+// This source file contains ...
+//
+//------------------------------------------------------------------------------------------------------------
+//
+// ... mention whee it came from ...
+//
+//
+// LCS - OLED Display Driver - Raspberry PI Pico Implementation
+// Copyright (C) 2024- 2024  Helmut Fieres
+//
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General
+// Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along with this program. If not, see
+// http://www.gnu.org/licenses
+//
+//  GNU General Public License:  http://opensource.org/licenses/GPL-3.0
+//
+//------------------------------------------------------------------------------------------------------------
+
+#include "LcsOledDisplayLib.h"
+
+//------------------------------------------------------------------------------------------------------------
+// Local name space. This file has two sections. The first is this local name space with all internal
+// variables and routines local to the file. The second part contains the exported routines to be called by
+// the core library and the firmware designers.
+//
+//------------------------------------------------------------------------------------------------------------
+namespace {
+
+  //----------------------------------------------------------------------------------------------------------
+  //
+  //----------------------------------------------------------------------------------------------------------
+ 
+ 
+
+
+}; // namespace
+
+
+//------------------------------------------------------------------------------------------------------------
+//
+//
+//------------------------------------------------------------------------------------------------------------
+
+
+//------------------------------------------------------------------------------------------------------------
+//
+//
+//------------------------------------------------------------------------------------------------------------
+
+
+  
---
+++ LcsLibraries/LcsRuntimeLib.md
@@ -0,0 +1,15 @@
+
+## Listings for runtime
+
+|File Name|Last Modified|
+|:---|:---|
+
+
+<div style="page-break-before: always;"></div>
+
+
+
+
+<div style="page-break-before: always;"></div>
+
+
