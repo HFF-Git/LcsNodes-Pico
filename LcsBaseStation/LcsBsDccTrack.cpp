209226e3a24dd20ae50523232afd9399def06cca
Helmut Fieres <67054756+HFF-Git@users.noreply.github.com>
2024-09-19 09:37:12 +0200

    Fixed a typo

--- LcsBaseStation/LcsBsDccTrack.cpp
+++ LcsBaseStation/LcsBsDccTrack.cpp
@@ -10,7 +10,7 @@
 //------------------------------------------------------------------------------------------------------------
 //
 // LCS - Base Station DCC Track implementation file
-// Copyright (C) 2019 - 2023  Helmut Fieres
+// Copyright (C) 2019 - 2024  Helmut Fieres
 //
 // This program is free software: you can redistribute it and/or modify it under the terms of the GNU General
 // Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
